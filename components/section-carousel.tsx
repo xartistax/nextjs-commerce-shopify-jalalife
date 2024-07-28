5d2267788d6e46d940486a6d9dc2060dc198b5e9
Artista <demianfueglistaler@icloud..com>
2024-07-29 00:12:58 +0200

    fixed the rendering of the collection carousel content

--- components/section-carousel.tsx
+++ components/section-carousel.tsx
@@ -3,7 +3,7 @@ import { Box, Button, Container, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
-import TextTruncate from 'react-text-truncate';
+import styled from 'styled-components';
 
 interface Product {
   title: string;
@@ -13,6 +13,14 @@ interface Product {
   };
   handle: string;
 }
+
+const TruncatedText = styled(Typography)`
+  display: -webkit-box;
+  -webkit-line-clamp: 9;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
 
 const CollectionProducts = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -69,106 +77,96 @@ const CollectionProducts = () => {
       <Box sx={{ paddingTop: {xs: '30px', md: '100px'}, paddingBottom: {xs: '30px', md: '100px'} }}>
         {products.length > 0 ? (
           <>
-          <Slide
-            duration={5000}
-            transitionDuration={300}
-            indicators={false}
-            slidesToScroll={1}
-            slidesToShow={1}
-            responsive={responsiveSettings}
-          >
-            {products.map((product, index) => (
-              <Box
-                key={index}
-                className="each-slide"
-                sx={{
-                  textAlign: 'left',
-                  display: 'flex',
-                  minHeight: '400px',
-                  height: 'auto',
-                  flexDirection: { xs: 'column', sm: 'row' } // Adjust layout based on screen size
-                }}
-              >
-                {/* Image Box (hidden on extra-small screens) */}
+            <Slide
+              duration={5000}
+              transitionDuration={300}
+              indicators={false}
+              slidesToScroll={1}
+              slidesToShow={1}
+              responsive={responsiveSettings}
+            >
+              {products.map((product, index) => (
                 <Box
+                  key={index}
+                  className="each-slide"
                   sx={{
-                    position: 'relative',
-                    flex: '1',
-                    display: { xs: 'none', sm: 'block' } // Corrected property name
+                    textAlign: 'left',
+                    display: 'flex',
+                    minHeight: '400px',
+                    height: 'auto',
+                    flexDirection: { xs: 'column', sm: 'row' }
                   }}
                 >
                   <Box
-                    component="img"
                     sx={{
-                      maxWidth: '100%',
-                      width: '100%',
-                      height: 'auto',
-                      position: 'absolute',
-                      bottom: '0',
-                      left: '0'
+                      position: 'relative',
+                      flex: '1',
+                      display: { xs: 'none', sm: 'block' }
                     }}
-                    alt={product.title}
-                    src={product.featuredImage?.url}
-                    loading="lazy"
-                  />
-                </Box>
+                  >
+                    <Box
+                      component="img"
+                      sx={{
+                        maxWidth: '100%',
+                        width: '100%',
+                        height: 'auto',
+                        position: 'absolute',
+                        bottom: '0',
+                        left: '0'
+                      }}
+                      alt={product.title}
+                      src={product.featuredImage?.url}
+                      loading="lazy"
+                    />
+                  </Box>
 
-                {/* Text Box */}
-                <Box
-                  sx={{
-                    flex: '1',
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    paddingLeft: '16px'
-                  }}
-                >
-                  <div>
-                    <Typography
-                      gutterBottom
-                      variant="h4"
-                      component="h2"
-                      sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}
-                      className="text-slate-800"
-                    >
-                      {product.title}
-                    </Typography>
-                    <Typography
-                      component="p"
-                      className="text-slate-600"
-                      sx={{ textAlign: 'justify' }}
-                    >
-                      <TextTruncate
-                        line={9}
-                        element="span"
-                        truncateText="…"
-                        text={product.description}
-                        textTruncateChild={
-                          <Typography
-                            component="a"
-                            href={`/product/${product.handle}`}
-                            sx={{
-                              display: 'block',
-                              textAlign: {md: 'right', xs: 'left'},
-                              fontWeight: {md: 'inherit', xs: 'bold'},
-                              fontSize: '16px',
-                              textTransform: 'uppercase',
-                              paddingTop: {md: '50px', xs: '20px'},
-                              color: 'primary.main'
-                            }}
-                          >
-                            Zum Produkt
-                          </Typography>
-                        }
-                      />
-                    </Typography>
-                  </div>
+                  <Box
+                    sx={{
+                      flex: '1',
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      paddingLeft: '16px'
+                    }}
+                  >
+                    <div>
+                      <Typography
+                        gutterBottom
+                        variant="h4"
+                        component="h2"
+                        sx={{ fontWeight: 'bold', textTransform: 'uppercase' }}
+                        className="text-slate-800"
+                      >
+                        {product.title}
+                      </Typography>
+                      <TruncatedText
+                       
+                        className="text-slate-600"
+                        sx={{ textAlign: 'justify' }}
+                      >
+                        {product.description}
+                      </TruncatedText>
+                      <Typography
+                        component="a"
+                        href={`/product/${product.handle}`}
+                        sx={{
+                          display: 'block',
+                          textAlign: {md: 'right', xs: 'left'},
+                          fontWeight: {md: 'inherit', xs: 'bold'},
+                          fontSize: '16px',
+                          textTransform: 'uppercase',
+                          paddingTop: {md: '50px', xs: '20px'},
+                          color: 'primary.main'
+                        }}
+                      >
+                        Zum Produkt
+                      </Typography>
+                    </div>
+                  </Box>
                 </Box>
-                
-              </Box>
-            ))}
-          </Slide>
-          <Button variant='outlined' href='/collections/online-shop' size='large' sx={{ 'marginTop': '3rem', 'fontWeight': 'bold'}}> Jetzt zum Shop</Button>
+              ))}
+            </Slide>
+            <Button variant='outlined' href='/collections/online-shop' size='large' sx={{ 'marginTop': '3rem', 'fontWeight': 'bold'}}> Jetzt zum Shop</Button>
           </>
         ) : (
           <div>No products available</div>
