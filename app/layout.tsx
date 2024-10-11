765acea600b1b6a423f65136c8c89822b92271e5
xartistax <info@webcrunch.ch>
2024-10-11 18:22:50 +0200

    Console Log Errors and Promoproduct in blog singlepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from '@mui/material';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-import ShopifyAnalytics from 'components/shopify-analytics';
 import { ensureStartsWith } from 'lib/utils';
 import dynamic from 'next/dynamic';
 import localFont from 'next/font/local';
@@ -52,7 +51,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
           <AppRouterCacheProvider>
             <Navbar />
             <main className="--font-brandon-regular">{children}</main>
-            <ShopifyAnalytics />
+            {/* <ShopifyAnalytics /> */}
           </AppRouterCacheProvider>
         </body>
       </html>
