b7a7ce271386b780a4fb95348d70aeab2f746f62
xartistax <info@webcrunch.ch>
2025-03-10 09:53:06 +0100

    abo test

--- lib/shopify/shopify.d.ts
+++ lib/shopify/shopify.d.ts
@@ -12,12 +12,11 @@ interface ShopifyCustomerPrivacy {
 
 // eslint-disable-next-line no-unused-vars
 interface Shopify {
-  loadFeatures?: (
+  loadFeatures: (
     features: { name: string; version: string }[],
     callback: (error: boolean) => void
   ) => void;
-  customerPrivacy?: ShopifyCustomerPrivacy;  // Make it optional
-  shop?: string;  // Make the shop property optional
+  customerPrivacy: ShopifyCustomerPrivacy;
 }
 
 // eslint-disable-next-line no-unused-vars
